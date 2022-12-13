634221465ea547ef26a26ca06c7e6e5d42a981ad
Seth Hoenig <shoenig@duck.com>
2022-12-13 08:58:28 -0600

    wait: rename to Constraint and add docs

--- wait/wait.go
+++ wait/wait.go
@@ -20,21 +20,21 @@ const (
 	defaultGap     = 250 * time.Millisecond
 )
 
-type Option func(*Control)
+// Option is used to configure a Constraint.
+type Option func(*Constraint)
 
 type runnable func(*runner) *result
 
 type runner struct {
-	ctrl     *Control
+	c        *Constraint
 	attempts int
 }
 
 type result struct {
-	Err     error
-	Success bool
+	Err error
 }
 
-// todo: Control parent
+// todo: Context parent
 
 // Timeout sets the maximum amount of time to allow before giving up and marking
 // the result as a failure.
@@ -43,7 +43,7 @@ type result struct {
 //
 // Default 3 seconds.
 func Timeout(duration time.Duration) Option {
-	return func(c *Control) {
+	return func(c *Constraint) {
 		c.deadline = time.Now().Add(duration)
 		c.iterations = math.MaxInt64
 	}
@@ -56,7 +56,7 @@ func Timeout(duration time.Duration) Option {
 //
 // By default a max timeout is used and the number of attempts is unlimited.
 func Attempts(max int) Option {
-	return func(c *Control) {
+	return func(c *Constraint) {
 		c.iterations = max
 		c.deadline = time.Date(9999, 0, 0, 0, 0, 0, 0, time.UTC)
 	}
@@ -66,15 +66,15 @@ func Attempts(max int) Option {
 //
 // Default 250 milliseconds.
 func Gap(duration time.Duration) Option {
-	return func(c *Control) {
+	return func(c *Constraint) {
 		c.gap = duration
 	}
 }
 
-// BoolFunc will retry f while it returns false, or a wait context threshold is
-// exceeded.
+// BoolFunc will retry f while it returns false, or until a wait constraint
+// threshold is exceeded.
 func BoolFunc(f func() bool) Option {
-	return func(c *Control) {
+	return func(c *Constraint) {
 		c.r = boolFunc(f)
 	}
 }
@@ -82,20 +82,20 @@ func BoolFunc(f func() bool) Option {
 func boolFunc(f func() bool) runnable {
 	bg := context.Background()
 	return func(r *runner) *result {
-		ctx, cancel := context.WithDeadline(bg, r.ctrl.deadline)
+		ctx, cancel := context.WithDeadline(bg, r.c.deadline)
 		defer cancel()
 
 		for {
 			// make an attempt
 			if f() {
-				return &result{Success: true}
+				return &result{Err: nil}
 			}
 
 			// used another attempt
 			r.attempts++
 
 			// check iterations
-			if r.attempts > r.ctrl.iterations {
+			if r.attempts > r.c.iterations {
 				return &result{Err: ErrAttemptsExceeded}
 			}
 
@@ -103,15 +103,17 @@ func boolFunc(f func() bool) runnable {
 			select {
 			case <-ctx.Done():
 				return &result{Err: ErrTimeoutExceeded}
-			case <-time.After(r.ctrl.gap):
+			case <-time.After(r.c.gap):
 				// continue
 			}
 		}
 	}
 }
 
+// ErrorFunc will retry f while it returns a non-nil error, or until a wait
+// constraint threshold is exceeded.
 func ErrorFunc(f func() error) Option {
-	return func(c *Control) {
+	return func(c *Constraint) {
 		c.r = errorFunc(f)
 	}
 }
@@ -119,21 +121,21 @@ func ErrorFunc(f func() error) Option {
 func errorFunc(f func() error) runnable {
 	bg := context.Background()
 	return func(r *runner) *result {
-		ctx, cancel := context.WithDeadline(bg, r.ctrl.deadline)
+		ctx, cancel := context.WithDeadline(bg, r.c.deadline)
 		defer cancel()
 
 		for {
 			// make an attempt
 			err := f()
 			if err == nil {
-				return &result{Success: true}
+				return &result{Err: nil}
 			}
 
 			// used another attempt
 			r.attempts++
 
 			// check iterations
-			if r.attempts > r.ctrl.iterations {
+			if r.attempts > r.c.iterations {
 				return &result{
 					Err: fmt.Errorf("%v: %w", ErrAttemptsExceeded, err),
 				}
@@ -145,15 +147,18 @@ func errorFunc(f func() error) runnable {
 				return &result{
 					Err: fmt.Errorf("%v: %w", ErrTimeoutExceeded, err),
 				}
-			case <-time.After(r.ctrl.gap):
+			case <-time.After(r.c.gap):
 				// continue
 			}
 		}
 	}
 }
 
+// TestFunc will retry f while it returns false, or until a wait constraint
+// threshold is exceeded. If f never succeeds, the latest returned error is
+// wrapped into the result.
 func TestFunc(f func() (bool, error)) Option {
-	return func(c *Control) {
+	return func(c *Constraint) {
 		c.r = testFunc(f)
 	}
 }
@@ -161,14 +166,14 @@ func TestFunc(f func() (bool, error)) Option {
 func testFunc(f func() (bool, error)) runnable {
 	bg := context.Background()
 	return func(r *runner) *result {
-		ctx, cancel := context.WithDeadline(bg, r.ctrl.deadline)
+		ctx, cancel := context.WithDeadline(bg, r.c.deadline)
 		defer cancel()
 
 		for {
 			// make an attempt
 			ok, err := f()
 			if ok {
-				return &result{Success: true}
+				return &result{Err: nil}
 			}
 
 			// set default error
@@ -180,7 +185,7 @@ func testFunc(f func() (bool, error)) runnable {
 			r.attempts++
 
 			// check iterations
-			if r.attempts > r.ctrl.iterations {
+			if r.attempts > r.c.iterations {
 				return &result{
 					Err: fmt.Errorf("%v: %w", ErrAttemptsExceeded, err),
 				}
@@ -192,15 +197,20 @@ func testFunc(f func() (bool, error)) runnable {
 				return &result{
 					Err: fmt.Errorf("%v: %w", ErrTimeoutExceeded, err),
 				}
-			case <-time.After(r.ctrl.gap):
+			case <-time.After(r.c.gap):
 				// continue
 			}
 		}
 	}
 }
 
-func On(opts ...Option) *Control {
-	c := &Control{now: time.Now()}
+// On creates a new Constraint with configuration set by opts.
+//
+// Timeout is used to set a maximum amount of time to wait for success.
+// Attempts is used to set a maximum number of attempts to wait for success.
+// Gap is used to control the amount of time to wait between retries.
+func On(opts ...Option) *Constraint {
+	c := &Constraint{now: time.Now()}
 	for _, opt := range append([]Option{
 		Timeout(defaultTimeout),
 		Gap(defaultGap),
@@ -210,7 +220,12 @@ func On(opts ...Option) *Control {
 	return c
 }
 
-type Control struct {
+// A Constraint is something a test assertions can wait on before marking the
+// result to be a failure. A Constraint that uses Timeout will retry a function
+// until the timeout expires. A Constraint that uses Attempts will retry a function
+// until the number of attempts is exhausted. The interval between retry attempts
+// can be configured with Gap.
+type Constraint struct {
 	now        time.Time
 	deadline   time.Time
 	gap        time.Duration
@@ -218,12 +233,13 @@ type Control struct {
 	r          runnable
 }
 
-func (ctrl *Control) Run() error {
-	if ctrl.r == nil {
+// Run the Constraint and produce an error result.
+func (c *Constraint) Run() error {
+	if c.r == nil {
 		return ErrNoFunction
 	}
-	return ctrl.r(&runner{
-		ctrl:     ctrl,
+	return c.r(&runner{
+		c:        c,
 		attempts: 0,
 	}).Err
 }
