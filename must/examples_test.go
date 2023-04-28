fd4ddf619d8ba65bded9a535bf35d235fb1fa98b
Seth Hoenig <shoenig@duck.com>
2023-04-28 14:20:36 -0500

    examples: start on runnable Go doc examples (#122)

---
+++ must/examples_test.go
@@ -0,0 +1,331 @@
+// Code generated via scripts/generate.sh. DO NOT EDIT.
+
+package must
+
+import (
+	"fmt"
+	"strings"
+)
+
+var t = new(myT)
+
+// myT is a substitute for testing.T for use in examples
+type myT struct{}
+
+func (t *myT) Errorf(s string, args ...any) {
+	s = fmt.Sprintf(s, args...)
+	fmt.Println(s)
+}
+
+func (t *myT) Fatalf(s string, args ...any) {
+	s = fmt.Sprintf(s, args...)
+	fmt.Println(s)
+}
+
+func (t *myT) Helper() {
+	// nothing
+}
+
+type myContainer[T comparable] struct {
+	items map[T]struct{}
+}
+
+func newContainer[T comparable](items ...T) *myContainer[T] {
+	c := &myContainer[T]{items: make(map[T]struct{})}
+	for _, item := range items {
+		c.items[item] = struct{}{}
+	}
+	return c
+}
+
+func (c *myContainer[T]) Contains(item T) bool {
+	_, exists := c.items[item]
+	return exists
+}
+
+func (c *myContainer[T]) Empty() bool {
+	return len(c.items) == 0
+}
+
+func ExampleAscending() {
+	nums := []int{1, 3, 4, 4, 9}
+	Ascending(t, nums)
+	// Output:
+}
+
+// AscendingCmp
+
+// AscendingFunc
+
+// AscendingLess
+
+func ExampleBetween() {
+	lower, upper := 3, 9
+	value := 5
+	Between(t, lower, value, upper)
+	// Output:
+}
+
+func ExampleBetweenExclusive() {
+	lower, upper := 2, 8
+	value := 4
+	BetweenExclusive(t, lower, value, upper)
+	// Output:
+}
+
+func ExampleContains() {
+	// container implements .Contains method
+	container := newContainer(2, 4, 6, 8)
+	Contains[int](t, 4, container)
+	// Output:
+}
+
+func ExampleContainsSubset() {
+	// container implements .Contains method
+	container := newContainer(1, 2, 3, 4, 5, 6)
+	ContainsSubset[int](t, []int{2, 4, 6}, container)
+	// Output:
+}
+
+func ExampleDescending() {
+	nums := []int{9, 6, 5, 4, 4, 2, 1}
+	Descending(t, nums)
+	// Output:
+}
+
+func ExampleDescendingCmp() {
+	nums := []int{9, 5, 3, 3, 1, -2}
+	DescendingCmp(t, nums, func(a, b int) int {
+		return a - b
+	})
+	// Output:
+}
+
+func ExampleDescendingFunc() {
+	words := []string{"Foo", "baz", "Bar", "AND"}
+	DescendingFunc(t, words, func(a, b string) bool {
+		lowerA := strings.ToLower(a)
+		lowerB := strings.ToLower(b)
+		return lowerA < lowerB
+	})
+	// Output:
+}
+
+// DescendingLess
+
+// DirExists
+
+// DirExistsFS
+
+// DirNotExists
+
+// DirNotExistsFS
+
+func ExampleEmpty() {
+	// container implements .Empty method
+	container := newContainer[string]()
+	Empty(t, container)
+	// Output:
+}
+
+func ExampleEq() {
+	actual := "hello"
+	Eq(t, "hello", actual)
+	// Output:
+}
+
+// EqError
+
+// EqFunc
+
+// EqJSON
+
+// EqOp
+
+// Equal
+
+// Error
+
+// ErrorContains
+
+// ErrorIs
+
+// False
+
+// FileContains
+
+// FileContainsFS
+
+// FileExists
+
+// FileExistsFS
+
+// FileMode
+
+// FileModeFS
+
+// FileNotExists
+
+// FileNotExistsFS
+
+// FilePathValid
+
+// Greater
+
+// GreaterEq
+
+// InDelta
+
+// InDeltaSlice
+
+// Len
+
+// Length
+
+// Less
+
+// LessEq
+
+// Lesser
+
+// MapContainsKey
+
+// MapContainsKeys
+
+// MapContainsValues
+
+// MapContainsValuesEqual
+
+// MapContainsValuesFunc
+
+// MapEmpty
+
+// MapEq
+
+// MapEqFunc
+
+// MapEqual
+
+// MapLen
+
+// MapNotContainsKey
+
+// MapNotContainsKeys
+
+// MapNotContainsValues
+
+// MapNotContainsValuesEqual
+
+// MapNotContainsValuesFunc
+
+// MapNotEmpty
+
+// Max
+
+// Min
+
+// Negative
+
+// Nil
+
+// NoError
+
+// NonNegative
+
+// NonPositive
+
+// NonZero
+
+// NotContains
+
+// NotEmpty
+
+// NotEq
+
+// NotEqFunc
+
+// NotEqOp
+
+// NotEqual
+
+// NotNil
+
+// One
+
+// Positive
+
+// RegexCompiles
+
+// RegexCompilesPOSIX
+
+// RegexMatch
+
+// Size
+
+// SliceContains
+
+// SliceContainsAll
+
+// SliceContainsEqual
+
+// SliceContainsFunc
+
+// SliceContainsOp
+
+// SliceContainsSubset
+
+// SliceEmpty
+
+// SliceEqFunc
+
+// SliceEqual
+
+// SliceLen
+
+// SliceNotContains
+
+// SliceNotContainsFunc
+
+// SliceNotEmpty
+
+// StrContains
+
+// StrContainsAny
+
+// StrContainsFields
+
+// StrContainsFold
+
+// StrCount
+
+// StrEqFold
+
+// StrHasPrefix
+
+// StrHasSuffix
+
+// StrNotContains
+
+// StrNotContainsAny
+
+// StrNotContainsFold
+
+// StrNotEqFold
+
+// StrNotHasPrefix
+
+// StructEqual
+
+// True
+
+// UUIDv4
+
+// Unreachable
+
+// ValidJSON
+
+// ValidJSONBytes
+
+// Wait
+
+// Zero
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -10,6 +10,7 @@ cp scripts.go must/scripts.go
 cp scripts_test.go must/scripts_test.go
 cp test.go must/must.go
 cp test_test.go must/must_test.go
+cp examples_test.go must/examples_test.go
 sed -i "s|package test|package must|g" must/invocations.go
 sed -i "s|package test|package must|g" must/invocations_test.go
 sed -i "s|package test|package must|g" must/settings.go
@@ -18,6 +19,7 @@ sed -i "s|package test|package must|g" must/scripts.go
 sed -i "s|package test|package must|g" must/scripts_test.go
 sed -i "s|package test|package must|g" must/must.go
 sed -i "s|package test|package must|g" must/must_test.go
+sed -i "s|package test|package must|g" must/examples_test.go
 sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" must/invocations.go
 sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" must/invocations_test.go
 sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" must/settings.go
@@ -26,3 +28,5 @@ sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" m
 sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" must/scripts_test.go
 sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" must/must.go
 sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" must/must_test.go
+sed -i -e "1s|^|// Code generated via scripts/generate.sh. DO NOT EDIT.\n\n|g" must/examples_test.go
+
